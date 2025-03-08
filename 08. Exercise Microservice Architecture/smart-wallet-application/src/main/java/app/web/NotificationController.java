06c0c06ab29a4a460294615078cb6979bbb33ff2
Dimitar-Peev <mctrix87@abv.bg>
2025-03-08 13:50:42 +0200

    Add "NotificationController.java"

---
+++ 08. Exercise Microservice Architecture/smart-wallet-application/src/main/java/app/web/NotificationController.java
@@ -0,0 +1,61 @@
+package app.web;
+
+import app.notification.client.dto.Notification;
+import app.notification.client.dto.NotificationPreference;
+import app.notification.service.NotificationService;
+import app.security.AuthenticationMetadata;
+import app.user.model.User;
+import app.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/notifications")
+public class NotificationController {
+
+    private final UserService userService;
+    private final NotificationService notificationService;
+
+    @Autowired
+    public NotificationController(UserService userService, NotificationService notificationService) {
+        this.userService = userService;
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping
+    public ModelAndView getNotificationPage(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
+
+        User user = userService.getById(authenticationMetadata.getUserId());
+
+        NotificationPreference notificationPreference = notificationService.getNotificationPreference(user.getId());
+        List<Notification> notificationHistory = notificationService.getNotificationHistory(user.getId());
+        notificationHistory = notificationHistory.stream().limit(5).toList();
+        long succeededNotificationsNumber = notificationHistory.stream().filter(notification -> notification.getStatus().equals("SUCCEEDED")).count();
+        long failedNotificationsNumber = notificationHistory.stream().filter(notification -> notification.getStatus().equals("FAILED")).count();
+
+        ModelAndView modelAndView = new ModelAndView("notifications");
+        modelAndView.addObject("user", user);
+        modelAndView.addObject("notificationPreference", notificationPreference);
+        modelAndView.addObject("notificationHistory", notificationHistory);
+        modelAndView.addObject("succeededNotificationsNumber", succeededNotificationsNumber);
+        modelAndView.addObject("failedNotificationsNumber", failedNotificationsNumber);
+
+        return modelAndView;
+    }
+
+    @PutMapping("/user-preference")
+    public String updateUserPreference(@RequestParam(name = "enabled") boolean enabled, @AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
+
+        notificationService.updateNotificationPreference(authenticationMetadata.getUserId(), enabled);
+
+        return "redirect:/notifications";
+    }
+}
