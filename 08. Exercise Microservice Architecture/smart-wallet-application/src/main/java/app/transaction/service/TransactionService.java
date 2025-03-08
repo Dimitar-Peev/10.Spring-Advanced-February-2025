7e63292a474a7ee41586380e9f05c3545742a647
Dimitar-Peev <mctrix87@abv.bg>
2025-03-08 13:49:42 +0200

    Update "TransactionService.java"

---
+++ 08. Exercise Microservice Architecture/smart-wallet-application/src/main/java/app/transaction/service/TransactionService.java
@@ -0,0 +1,99 @@
+package app.transaction.service;
+
+import app.exception.DomainException;
+import app.notification.service.NotificationService;
+import app.transaction.model.Transaction;
+import app.transaction.model.TransactionStatus;
+import app.transaction.model.TransactionType;
+import app.transaction.repository.TransactionRepository;
+import app.user.model.User;
+import app.wallet.model.Wallet;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Currency;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final NotificationService notificationService;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository, NotificationService notificationService) {
+        this.transactionRepository = transactionRepository;
+        this.notificationService = notificationService;
+    }
+
+    public Transaction createNewTransaction(User owner, String sender, String receiver, BigDecimal transactionAmount, BigDecimal balanceLeft, Currency currency, TransactionType type, TransactionStatus status, String transactionDescription, String failureReason) {
+
+        Transaction transaction = Transaction.builder()
+                .owner(owner)
+                .sender(sender)
+                .receiver(receiver)
+                .amount(transactionAmount)
+                .balanceLeft(balanceLeft)
+                .currency(currency)
+                .type(type)
+                .status(status)
+                .description(transactionDescription)
+                .failureReason(failureReason)
+                .createdOn(LocalDateTime.now())
+                .build();
+
+        String emailBody = "%s transaction was successful processed for you with amount %.2f EUR!".formatted(transaction.getType(), transaction.getAmount());
+        notificationService.sendNotification(transaction.getOwner().getId(), "New Smart Wallet Transaction", emailBody);
+
+        return transactionRepository.save(transaction);
+    }
+
+    public List<Transaction> getAllByOwnerId(UUID ownerId) {
+
+        return transactionRepository.findAllByOwnerIdOrderByCreatedOnDesc(ownerId);
+    }
+
+    public Transaction getById(UUID id) {
+
+        return transactionRepository.findById(id)
+                .orElseThrow(() -> new DomainException("Transaction with id [%s] does not exist.".formatted(id)));
+    }
+
+    public List<Transaction> getLastFourTransactionsByWallet(Wallet wallet) {
+
+        List<Transaction> lastFourTransactions = transactionRepository.findAllBySenderOrReceiverOrderByCreatedOnDesc(wallet.getId().toString(), wallet.getId().toString())
+                .stream()
+                .filter(t -> t.getOwner().getId() == wallet.getOwner().getId())
+                .filter(t -> t.getStatus() == TransactionStatus.SUCCEEDED)
+                .limit(4)
+                .collect(Collectors.toList());
+
+//        List<Transaction> allTransactions = transactionRepository.findAllBySenderOrReceiverOrderByCreatedOnDesc(
+//                wallet.getId().toString(), wallet.getId().toString());
+//
+//        List<Transaction> lastFourTransactions = new ArrayList<>();
+//        int count = 0;
+//
+//        for (Transaction t : allTransactions) {
+//            UUID id1 = t.getOwner().getId();
+//            UUID id2 = wallet.getOwner().getId();
+//            boolean b = t.getOwner().getId() == wallet.getOwner().getId();
+//
+//            if (t.getOwner().getId().equals(wallet.getOwner().getId()) && t.getStatus() == TransactionStatus.SUCCEEDED) {
+//                lastFourTransactions.add(t);
+//                count++;
+//                if (count == 4) {
+//                    break;
+//                }
+//            }
+//        }
+
+        return lastFourTransactions;
+    }
+}
