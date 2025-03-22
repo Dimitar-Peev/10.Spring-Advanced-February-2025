ecee75c30136dabda37734370707e4d5fd807b7e
Dimitar-Peev <mctrix87@abv.bg>
2025-03-22 22:37:31 +0200

    Update "UserServiceUTest.java"

--- 11. Unit Testing/smart-wallet-application/src/test/java/app/user/UserServiceUTest.java
+++ 11. Unit Testing/smart-wallet-application/src/test/java/app/user/UserServiceUTest.java
@@ -1,19 +1,26 @@
 package app.user;
 
 import app.exception.DomainException;
+import app.exception.UsernameAlreadyExistException;
 import app.notification.service.NotificationService;
+import app.security.AuthenticationMetadata;
+import app.subscription.model.Subscription;
 import app.subscription.service.SubscriptionService;
+import app.user.model.Country;
 import app.user.model.User;
 import app.user.model.UserRole;
 import app.user.repository.UserRepository;
 import app.user.service.UserService;
+import app.wallet.model.Wallet;
 import app.wallet.service.WalletService;
+import app.web.dto.RegisterRequest;
 import app.web.dto.UserEditRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
@@ -21,8 +28,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 // 1. Create the test class
@@ -60,6 +66,8 @@ public class UserServiceUTest {
         assertThrows(DomainException.class, () -> userService.editUserDetails(userId, dto));
     }
 
+    // Test Case: When database returns user object -> then change their details from the dto with email address
+    // and save notification preference and save the user to the database
     @Test
     void givenExistingUser_whenEditTheirProfileWithActualEmail_thenChangeTheirDetailsSaveNotificationPreferenceAndSaveToDatabase() {
 
@@ -84,6 +92,156 @@ public class UserServiceUTest {
         assertEquals("www.image.com", user.getProfilePicture());
         verify(notificationService, times(1)).saveNotificationPreference(userId, true, dto.getEmail());
         verify(userRepository, times(1)).save(user);
+    }
+
+    @Test
+    void givenExistingUser_whenEditTheirProfileWithEmptyEmail_thenChangeTheirDetailsSaveNotificationPreferenceAndSaveToDatabase() {
+
+        // Given
+        UUID userId = UUID.randomUUID();
+        UserEditRequest dto = UserEditRequest.builder()
+                .firstName("Dimitar")
+                .lastName("Peev")
+                .email("")
+                .profilePicture("www.image.com")
+                .build();
+        User user = User.builder().build();
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        // When
+        userService.editUserDetails(userId, dto);
+
+        // Then
+        assertEquals("Dimitar", user.getFirstName());
+        assertEquals("Peev", user.getLastName());
+        assertEquals("", user.getEmail());
+        assertEquals("www.image.com", user.getProfilePicture());
+        verify(notificationService, times(1)).saveNotificationPreference(userId, false, null);
+        verify(userRepository, times(1)).save(user);
+    }
+
+    // Test 2: When User does not exist - then throws exception
+    @Test
+    void givenMissingUserFromDatabase_whenLoadUserByUsername_thenExceptionIsThrown() {
+
+        // Given
+        String username = "Dimitar";
+        when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(DomainException.class, () -> userService.loadUserByUsername(username));
+    }
+
+    // Test 1: When user exist - then return new AuthenticationMetadata
+    @Test
+    void givenExistingUser_whenLoadUserByUsername_thenReturnCorrectAuthenticationMetadata() {
+
+        // Given
+        String username = "Dimitar";
+        User user = User.builder()
+                .id(UUID.randomUUID())
+                .isActive(true)
+                .password("123456")
+                .role(UserRole.ADMIN)
+                .build();
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
+
+        // When
+        UserDetails authenticationMetadata = userService.loadUserByUsername(username);
+
+        // Then
+        assertInstanceOf(AuthenticationMetadata.class, authenticationMetadata);
+        AuthenticationMetadata result = (AuthenticationMetadata) authenticationMetadata;
+        assertEquals(user.getId(), result.getUserId());
+        assertEquals(username, result.getUsername());
+        assertEquals(user.getPassword(), result.getPassword());
+        assertEquals(user.isActive(), result.isActive());
+        assertEquals(user.getRole(), result.getRole());
+        assertThat(result.getAuthorities()).hasSize(1);
+        assertEquals("ROLE_ADMIN", result.getAuthorities().iterator().next().getAuthority());
+    }
+
+    // Register
+    // Test 1: When user exist with this username -> exception is thrown
+    @Test
+    void givenExistingUsername_whenRegister_thenExceptionIsThrown() {
+
+        // Given
+        RegisterRequest registerRequest = RegisterRequest.builder()
+                .username("Dimitar")
+                .password("123456")
+                .country(Country.BULGARIA)
+                .build();
+        when(userRepository.findByUsername(any())).thenReturn(Optional.of(new User()));
+
+        // When & Then
+        assertThrows(UsernameAlreadyExistException.class, () -> userService.register(registerRequest));
+        verify(userRepository, never()).save(any());
+        verify(subscriptionService, never()).createDefaultSubscription(any());
+        verify(walletService, never()).initilizeFirstWallet(any());
+        verify(notificationService, never()).saveNotificationPreference(any(UUID.class), anyBoolean(), anyString());
+    }
+
+    // Test 2: Happy path Registration
+    @Test
+    void givenHappyPath_whenRegister() {
+
+        // Given
+        RegisterRequest registerRequest = RegisterRequest.builder()
+                .username("Dimitar")
+                .password("123456")
+                .country(Country.BULGARIA)
+                .build();
+        User user = User.builder()
+                .id(UUID.randomUUID())
+                .build();
+        when(userRepository.findByUsername(registerRequest.getUsername())).thenReturn(Optional.empty());
+        when(userRepository.save(any())).thenReturn(user);
+        when(subscriptionService.createDefaultSubscription(user)).thenReturn(new Subscription());
+        when(walletService.initilizeFirstWallet(user)).thenReturn(new Wallet());
+
+        // When
+        User registeredUser = userService.register(registerRequest);
+
+        // Then
+        assertThat(registeredUser.getSubscriptions()).hasSize(1);
+        assertThat(registeredUser.getWallets()).hasSize(1);
+        verify(notificationService, times(1)).saveNotificationPreference(user.getId(), false, null);
+    }
+
+
+
+
+
+
+
+
+
+
+    @Test
+    void givenExistingUsersInDatabase_whenGetAllUsers_thenReturnThemAll() {
+
+        // Give
+        List<User> userList = List.of(new User(), new User());
+        when(userRepository.findAll()).thenReturn(userList);
+
+        // When
+        List<User> users = userService.getAllUsers();
+
+        // Then
+        assertThat(users).hasSize(2);
+    }
+
+    @Test
+    public void whenRepositoryReturnEmptyOptional_thenExceptionIsThrown(){
+
+        // Given
+        UUID userId = UUID.randomUUID();
+        UserEditRequest dto = UserEditRequest.builder().build();
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(DomainException.class, () -> userService.editUserDetails(userId, dto));
     }
 
     @Test
@@ -120,19 +278,5 @@ public class UserServiceUTest {
 
         // Then
         assertThat(user.getRole()).isEqualTo(UserRole.ADMIN);
-    }
-
-    @Test
-    void givenExistingUsersInDatabase_whenGetAllUsers_thenReturnThemAll() {
-
-        // Give
-        List<User> userList = List.of(new User(), new User());
-        when(userRepository.findAll()).thenReturn(userList);
-
-        // When
-        List<User> users = userService.getAllUsers();
-
-        // Then
-        assertThat(users).hasSize(2);
     }
 }
