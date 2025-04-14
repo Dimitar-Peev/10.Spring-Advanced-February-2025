fa69aa0bc5f4b3f764fe3a10bf15b2f55492db33
Dimitar-Peev <mctrix87@abv.bg>
2025-04-14 07:04:00 +0300

    Updated "UserControllerApiTest.java" -> added two tests

--- 12. Integration and API Testing/smart-wallet-application/src/test/java/app/web/UserControllerApiTest.java
+++ 12. Integration and API Testing/smart-wallet-application/src/test/java/app/web/UserControllerApiTest.java
@@ -62,4 +62,37 @@ public class UserControllerApiTest {
         verify(userService, times(1)).switchRole(any());
     }
 
+    @Test
+    void putUnauthorizedRequestToSwitchStatus_shouldReturn404AndNotFoundView() throws Exception {
+
+        // 1. Build Request
+        AuthenticationMetadata principal = new AuthenticationMetadata(UUID.randomUUID(), "User123", "123123", UserRole.USER, true);
+        MockHttpServletRequestBuilder request = put("/users/{id}/status", UUID.randomUUID())
+                .with(user(principal))
+                .with(csrf());
+
+        // 2. Send Request
+        mockMvc.perform(request)
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("not-found"));
+    }
+
+    @Test
+    void putAuthorizedRequestToSwitchStatus_shouldRedirectToUsers() throws Exception {
+
+        // 1. Build Request
+        AuthenticationMetadata principal = new AuthenticationMetadata(UUID.randomUUID(), "User123", "123123", UserRole.ADMIN, true);
+        MockHttpServletRequestBuilder request = put("/users/{id}/status", UUID.randomUUID())
+                .with(user(principal))
+                .with(csrf());
+
+        // 2. Send Request
+        mockMvc.perform(request)
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/users"));
+
+        // 3. Verify that the switchRole method in userService was called once with any argument
+        verify(userService, times(1)).switchStatus(any());
+    }
+
 }
