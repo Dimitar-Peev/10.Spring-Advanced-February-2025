f085acd43da23baf00fe74a1512eab9a9c4dc5ea
Dimitar-Peev <mctrix87@abv.bg>
2025-06-08 10:51:11 +0300

    Updated "IndexController.java"

---
+++ 13. Aspect Oriented Programming/smart-wallet-application/src/main/java/app/web/IndexController.java
@@ -0,0 +1,103 @@
+package app.web;
+
+import app.security.AuthenticationMetadata;
+import app.user.model.User;
+import app.user.service.UserService;
+import app.web.dto.LoginRequest;
+import app.web.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    private final UserService userService;
+
+    @Autowired
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Send @GetMapping("/")
+    // Result -> view name "index"
+    @GetMapping("/")
+    public String getIndexPage() {
+
+
+        return "index";
+    }
+
+    // JoinPoint
+    @GetMapping("/login")
+    public ModelAndView getLoginPage(@RequestParam(value = "error", required = false) String errorParam) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        modelAndView.addObject("loginRequest", new LoginRequest());
+
+        if (errorParam != null) {
+            modelAndView.addObject("errorMessage", "Incorrect username or password!");
+        }
+
+        return modelAndView;
+    }
+
+    // JoinPoint
+    @GetMapping("/register")
+    public ModelAndView getRegisterPage() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("register");
+        modelAndView.addObject("registerRequest", new RegisterRequest());
+
+        return modelAndView;
+    }
+
+    // POST with correct form data
+    // Expect:
+    // status - 200 OK
+    // called .register method of userService
+    // redirect to /login
+    @PostMapping("/register")
+    public ModelAndView registerNewUser(@Valid RegisterRequest registerRequest, BindingResult bindingResult) {
+
+        // DEMO - Exception Handling -> @ExceptionHandler in the Controller
+//        throw new UsernameAlreadyExistException("Username already exist!");
+
+        if (bindingResult.hasErrors()) {
+
+            return new ModelAndView("register");
+        }
+
+        userService.register(registerRequest);
+
+        return new ModelAndView("redirect:/login");
+    }
+
+    @GetMapping("/home")
+    public ModelAndView getHomePage(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
+
+        User user = userService.getById(authenticationMetadata.getUserId());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        modelAndView.addObject("user", user);
+
+        return modelAndView;
+    }
+
+    // DEMO - Exception Handling -> @ExceptionHandler in the Controller
+//    @ExceptionHandler(UsernameAlreadyExistException.class)
+//    public String handleException() {
+//
+//        return "not-found";
+//    }
+
+}
